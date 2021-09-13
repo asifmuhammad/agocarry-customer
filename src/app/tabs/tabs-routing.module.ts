ed306c34c80848c877893c3aa2f3c6b6bd70de92
tasawar343 <89405744+tasawar343@users.noreply.github.com>
2021-09-13 21:45:53 +0500

    ui design

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -56,16 +56,27 @@ const routes: Routes = [
       ]
     },
     {
+      path: 'History',
+      children: [
+        {
+          path: '',
+          loadChildren: () =>
+            import('../orders/orders.module').then(m => m.OrdersPageModule)
+        }
+      ]
+    },
+
+    {
       path: 'customer-wallet',
       loadChildren: () => import('../main-home/customer-wallet/customer-wallet.module').then( m => m.CustomerWalletPageModule)
     },
-    {
-      path: 'customer-history',
-      loadChildren: () => import('../main-home/customer-history/customer-history.module').then( m => m.CustomerHistoryPageModule)
-    },
+    // {
+    //   path: 'customer-history',
+    //   loadChildren: () => import('../main-home/customer-history/customer-history.module').then( m => m.CustomerHistoryPageModule)
+    // },
     {
       path: 'customer-profile',
-      loadChildren: () => import('../main-home/customer-profile/customer-profile.module').then( m => m.CustomerProfilePageModule)
+      loadChildren: () => import('../account/account.module').then(m => m.AccountPageModule)
     }
     , {
       path: '',
